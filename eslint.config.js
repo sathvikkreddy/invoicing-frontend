ed595e7b776030d7de4417e081e676c895201994
Sathvik Reddy Kolla <ksathvikreddy@actalentservices.com>
2025-03-31 15:46:02 +0530

    changed linting rules to warn instead of error

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,8 @@ export default tseslint.config(
     ],
     rules: {
       "@typescript-eslint/array-type": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unsafe-member-access": "warn",
       "@typescript-eslint/consistent-type-definitions": "off",
       "@typescript-eslint/consistent-type-imports": [
         "warn",
