8f52a7b153c774b3a06e5290f350c565988ec03a
Sathvik Reddy Kolla <ksathvikreddy@actalentservices.com>
2025-04-12 10:51:16 +0530

    added invoices page

--- src/app/(auth)/invoices/page.tsx
+++ src/app/(auth)/invoices/page.tsx
@@ -1,9 +1,13 @@
-import Link from "next/link";
-import { fetchMe } from "~/strapi/users";
+import { AddInvoiceDialog } from './components/AddInvoiceDialog'
+import { getCompanies } from '../companies/actions'
 
 const Invoices = async () => {
-  const me = await fetchMe();
-  return <Link href={"/"}>{JSON.stringify(me)}</Link>;
-};
+    const companiesResponse = await getCompanies()
+    return (
+        <div>
+            <AddInvoiceDialog companies={companiesResponse?.data ?? []} />
+        </div>
+    )
+}
 
-export default Invoices;
+export default Invoices
---
+++ src/app/unauthorized/page.tsx
@@ -0,0 +1,5 @@
+const Unauthorized = () => {
+  return <div>403 - Unauthorized</div>;
+};
+
+export default Unauthorized;
