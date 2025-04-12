8321de3aefb76b845169e65571ef0f72685eceda
Sathvik Reddy Kolla <ksathvikreddy@actalentservices.com>
2025-04-12 10:41:39 +0530

    fixed linting and formatting configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,50 +1,61 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import tseslint from "typescript-eslint";
+import { FlatCompat } from '@eslint/eslintrc'
+import tseslint from 'typescript-eslint'
 
 const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-});
+    baseDirectory: import.meta.dirname,
+})
 
 export default tseslint.config(
-  {
-    ignores: [".next"],
-  },
-  ...compat.extends("next/core-web-vitals"),
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    extends: [
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
-    ],
-    rules: {
-      "@typescript-eslint/array-type": "off",
-      "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/no-unsafe-member-access": "warn",
-      "@typescript-eslint/consistent-type-definitions": "off",
-      "@typescript-eslint/consistent-type-imports": [
-        "warn",
-        { prefer: "type-imports", fixStyle: "inline-type-imports" },
-      ],
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { argsIgnorePattern: "^_" },
-      ],
-      "@typescript-eslint/require-await": "off",
-      "@typescript-eslint/no-misused-promises": [
-        "error",
-        { checksVoidReturn: { attributes: false } },
-      ],
+    {
+        ignores: ['.next'],
     },
-  },
-  {
-    linterOptions: {
-      reportUnusedDisableDirectives: true,
+    ...compat.extends('next/core-web-vitals'),
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+        extends: [
+            ...tseslint.configs.recommended,
+            ...tseslint.configs.recommendedTypeChecked,
+            ...tseslint.configs.stylisticTypeChecked,
+        ],
+        rules: {
+            '@typescript-eslint/array-type': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-unsafe-member-access': 'off',
+            '@typescript-eslint/no-unsafe-assignment': 'off',
+            '@typescript-eslint/no-unsafe-argument': 'off',
+            '@typescript-eslint/no-unsafe-return': 'off',
+            '@typescript-eslint/no-unsafe-call': 'off',
+            '@typescript-eslint/consistent-type-definitions': 'off',
+            '@typescript-eslint/no-floating-promises': 'off',
+            '@typescript-eslint/no-redundant-type-constituents': 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
+            '@typescript-eslint/prefer-nullish-coalescing': 'off',
+            '@typescript-eslint/consistent-type-imports': 'off', // change this to below
+            // '@typescript-eslint/consistent-type-imports': [
+            //     'warn',
+            //     { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
+            // ],
+            // '@typescript-eslint/no-unused-vars': [
+            //     'warn',
+            //     { argsIgnorePattern: '^_' },
+            // ],
+            '@typescript-eslint/require-await': 'off',
+            // '@typescript-eslint/no-misused-promises': [
+            //     'error',
+            //     { checksVoidReturn: { attributes: false } },
+            // ],
+            '@typescript-eslint/no-misused-promises': 'off',
+        },
     },
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-      },
-    },
-  },
-);
+    {
+        linterOptions: {
+            reportUnusedDisableDirectives: true,
+        },
+        languageOptions: {
+            parserOptions: {
+                project: './tsconfig.json',
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    }
+)
